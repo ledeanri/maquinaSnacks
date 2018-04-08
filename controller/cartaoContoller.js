737428c63619594b896a69f4175d98b3cb228c66
leandro <ledeanri@gmail.com>
2018-04-08 00:05:11 -0300

    usando o moment

--- controller/cartaoContoller.js
+++ controller/cartaoContoller.js
@@ -1,4 +1,5 @@
 var Cartao = require('../models/cartao');
+var moment = require('moment');
 
 exports.mostrar_saldo = function(id, callback){
   Cartao.findById(id, function(error, cartao){
@@ -15,9 +16,10 @@ exports.carga_automatica = function(id, callback){
     if (error){
       callback({error: 'Não foi possível encontrar o cartão'});
     } else {
-      var data_hoje = new Date();
+      var data_hoje = moment().format('YYYY-MM-DD');
       if (cartao.ultima_recarga !== data_hoje){
         cartao.valor = "5,50";
+        cartao.ultima_recarga = data_hoje;
         cartao.save(function(error, cartao){
           if(error){
             callback({error: 'Não foi possivel salvar'});
